c328a11768d02f3aa44bcc6415003378f6ac0570
Khalefa <khalefa@al-ahmad.com>
2019-10-29 10:41:44 +0300

    ok

--- PYPIDownloaderApp/app.py
+++ PYPIDownloaderApp/app.py
@@ -489,12 +489,9 @@ def process_update():
             print (colored('Total to process less than Max Allowed, Changing total to: %d'% (Total_To_Process),'red'))
         print (colored("Processing Batch %d     of     %d"%(Batch_Index + 1,Total_Number_of_Batches)   ,'green'))
         itemBatch = To_Initial_Process_Sorted[starting_index:starting_index+Total_To_Process]
-        ProcessPools = Pool(processes=MaxItemsToProcess)
         # got the below from: https://stackoverflow.com/questions/41920124/multiprocessing-use-tqdm-to-display-a-progress-bar/45276885
-        list(tqdm.tqdm(ProcessPools.imap(DownloadAndProcessesItemJob,itemBatch),total = MaxItemsToProcess, ))
-
-        ProcessPools.close()
-        ProcessPools.join()
+        list(tqdm.tqdm(map(DownloadAndProcessesItemJob,itemBatch),total = MaxItemsToProcess, ))
+        
 
         starting_index += Total_To_Process
         Batch_Index += 1
