a53f4880a0f60c9984b9acf926bd8858ef1a3fc6
Khalefa <khalefa@al-ahmad.com>
2019-10-29 14:15:49 +0300

    ok

--- PYPIDownloaderApp/app.py
+++ PYPIDownloaderApp/app.py
@@ -489,7 +489,7 @@ def process_update():
             print (colored('Total to process less than Max Allowed, Changing total to: %d'% (Total_To_Process),'red'))
         print (colored("Processing Batch %d     of     %d"%(Batch_Index + 1,Total_Number_of_Batches)   ,'green'))
         itemBatch = To_Initial_Process_Sorted[starting_index:starting_index+Total_To_Process]
-        printIndex = 0
+        printIndex = 1
         packagesProcessString= "["
         for i in itemBatch:
             packagesProcessString += str(printIndex) + "-" + i + ", "
